d895cc7855204198103937e4078f3711882e448e
undefined <undefined>
2021-09-10 22:32:06 +0300

    updated dapp metadata

---
+++ packages/daap/scripts/deploy.ts
@@ -0,0 +1,13 @@
+import run from '@jamesives/github-pages-deploy-action';
+
+run({
+  token: process.env['GITHUB_TOKEN'],
+  branch: 'gh-pages',
+  folder: 'build',
+  hostname: 'github.com',
+  repositoryName: 'windingtree/lif2',
+  workspace: '.',
+  clean: true,
+  silent: false,
+  isTest: 0
+});
